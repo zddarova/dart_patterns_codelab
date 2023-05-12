e7b9895b297d00b31d744db4d02e5a7ee29daebe
Andrii Komarychev <lkomarichev@gmail.com>
2023-05-12 19:26:41 +0300

    completed step 9.1

--- lib/main.dart
+++ lib/main.dart
@@ -49,3 +49,33 @@ class DocumentScreen extends StatelessWidget {
     );
   }
 }
+
+class BlockWidget extends StatelessWidget {
+  final Block block;
+
+  const BlockWidget({
+    required this.block,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    TextStyle? textStyle;
+    switch (block.type) {
+      case 'h1':
+        textStyle = Theme.of(context).textTheme.displayMedium;
+      case 'p' || 'checkbox':
+        textStyle = Theme.of(context).textTheme.bodyMedium;
+      case _:
+        textStyle = Theme.of(context).textTheme.bodySmall;
+    }
+
+    return Container(
+      margin: const EdgeInsets.all(8),
+      child: Text(
+        block.text,
+        style: textStyle,
+      ),
+    );
+  }
+}
